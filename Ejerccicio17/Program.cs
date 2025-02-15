5be75e4d2296c89302d5f7e92c0f4f36f570ac2c
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:28:12 -0600

    Agregar archivos de proyecto.

---
+++ Ejerccicio17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static int Sumar(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    static void Main(string[] args)
+    {
+        // Solicitar los dos números al usuario
+        Console.Write("Ingrese el primer número: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        // Invocar la función y mostrar el resultado
+        int resultado = Sumar(numero1, numero2);
+        Console.WriteLine("La suma de los dos números es: " + resultado);
+    }
+}
